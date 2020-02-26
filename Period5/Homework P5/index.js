6432d52b7f99de5cee31700ed1a7934da03fe3e0
phquann <quankid14@gmail.com>
2020-02-26 19:09:22 +0700

    BTVN khong theo thu tu dau anh

--- Period5/Homework P5/index.js
+++ Period5/Homework P5/index.js
@@ -1,5 +1,5 @@
 // Bai1
-// const obj = { JS : 1 , Python : 2 , PHP : 3 };
+// const obj = {};
 // function reverseObject(object) {
 //        for (var key in obj) {
 //         obj[object[key]]= key;
@@ -18,4 +18,66 @@
 //       result.push(obj[item]);
 //     }
 //     return result;
+// }
+
+// Bai3
+// function getFrequencies(array) {
+//     return array.reduce((output, input) => {
+//       op[input] = op[input] || 0
+//       op[input]++
+//       return output
+//     },{})
+// }
+
+// Bai4
+// function mapLetters(string) {
+//     const array = string.split("");
+//     const result = array.reduce((accumulator, currentItem, currentIndex) => {
+//       if (!accumulator[currentItem]) {
+//         accumulator[currentItem] = [];
+//         accumulator[currentItem].push(currentIndex);
+//       } else {
+//         accumulator[currentItem].push(currentIndex);
+//       }
+//       return accumulator;
+//     }, {});
+//     return result;
+// }
+
+// Bai5
+// function khaBanh(str) {
+//     let newString = "";
+//     newString += str[0];
+//     for (let i = 1; i < str.length; i++) {
+//       if (
+//         newString[i - 1] === newString[i - 1].toUpperCase() &&
+//         newString[i - 1] !== " "
+//       ) {
+//         newStringing += str[i].toLowerCase();
+//       } else if (
+//         newString[i - 1] === newString[i - 1].toLowerCase() &&
+//         newString[i - 1] !== " "
+//       ) {
+//         newString += str[i].toUpperCase();
+//       } else if (newString[i - 1] === " ") {
+//         if (newString[i - 2] === newString[i - 2].toUpperCase()) {
+//           newString += str[i].toLowerCase();
+//         } else if (newString[i - 2] === newString[i - 2].toLowerCase()) {
+//           newString += str[i].toUpperCase();
+//         }
+//       }
+//     }
+//     return newString;
+// }  
+
+// Bai6
+// function censoredString(str) {
+//     const coder = "PythonPHPJavaC++GoLang";
+//     let newStr = "";
+//     for (let i = 0; i < str.length; i++) {
+//       if (coder.includes(str[i])) {
+//         newStr += "*";
+//       } else newStr += str[i];
+//     }
+//     return newStr;
 //   }
