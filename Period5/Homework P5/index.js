a62cc020663ee51b0175eaf2cfe1f31edbc484ea
phquann <quankid14@gmail.com>
2020-02-26 19:07:04 +0700

    BTVN buoi 5 khong theo thu tu dau a

---
+++ Period5/Homework P5/index.js
@@ -0,0 +1,21 @@
+// Bai1
+// const obj = { JS : 1 , Python : 2 , PHP : 3 };
+// function reverseObject(object) {
+//        for (var key in obj) {
+//         obj[object[key]]= key;
+//     }
+//     console.log(object);       
+//     return object 
+// }
+
+// Bai2
+// function keysAndValues(obj) {
+//     const result = [];
+//     for (const item in obj) {
+//       result.push(item);
+//     }
+//     for (const item in obj) {
+//       result.push(obj[item]);
+//     }
+//     return result;
+//   }
